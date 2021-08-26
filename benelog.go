b35400e7518810af96f6160b4d671810b1764d2e
Cole Arendt <cole.arendt@outlook.com>
2021-08-26 14:35:41 -0400

    add benelog initial pass

---
+++ benelog.go
@@ -0,0 +1,84 @@
+package benelog
+
+import (
+    "context"
+    "go.uber.org/zap/zapcore"
+    "os"
+    "path"
+
+    "go.uber.org/zap"
+)
+
+type correlationIdType int
+const (
+    requestIdKey correlationIdType = iota
+    sessionIdKey
+)
+var logger zap.Logger
+
+func NewLogger(options ...zap.Option) (*zap.Logger, error) {
+    logConfig := zapcore.EncoderConfig{
+        MessageKey:     "msg",
+        LevelKey:       "level",
+        NameKey:        "name",
+        TimeKey:        "ts",
+        CallerKey:      "caller",
+        FunctionKey:    "func",
+        StacktraceKey:  "stacktrace",
+        LineEnding:     "\n",
+        EncodeTime:     zapcore.EpochTimeEncoder,
+        EncodeLevel:    zapcore.LowercaseLevelEncoder,
+        EncodeDuration: zapcore.SecondsDurationEncoder,
+        EncodeCaller:   zapcore.ShortCallerEncoder,
+    }
+    cfg := zap.Config{
+        Level:             zap.NewAtomicLevelAt(zapcore.InfoLevel),
+        Development:       false,
+        DisableCaller:     false,
+        DisableStacktrace: false,
+        Sampling:          nil,
+        Encoding:          "json",
+        EncoderConfig:     logConfig,
+        OutputPaths:       nil,
+        ErrorOutputPaths:  nil,
+        InitialFields:     nil,
+    }
+    fields := zap.Fields(zap.Int("pid", os.Getpid()),
+        zap.String("exe", path.Base(os.Args[0])))
+    options = append(options, fields)
+
+    return cfg.Build(options...)
+}
+
+func init() {
+    tmpLogger, err := NewLogger()
+    print("Hello friend")
+    if err != nil {
+    	panic("error creating simple logger")
+    }
+    logger = *tmpLogger
+}
+
+// WithRqId returns a context which knows its request ID
+func WithRqId(ctx context.Context, rqId string) context.Context {
+    return context.WithValue(ctx, requestIdKey, rqId)
+}
+
+// WithSessionId returns a context which knows its session ID
+func WithSessionId(ctx context.Context, sessionId string) context.Context {
+    return context.WithValue(ctx, sessionIdKey, sessionId)
+}
+
+// Logger returns a zap logger with as much context as possible
+func Logger(ctx context.Context) zap.Logger {
+    newLogger := logger
+    if ctx != nil {
+        if ctxRqId, ok := ctx.Value(requestIdKey).(string); ok {
+            newLogger = *newLogger.With(zap.String("rqId", ctxRqId))
+        }
+        if ctxSessionId, ok := ctx.Value(sessionIdKey).(string); ok {
+           newLogger = *newLogger.With(zap.String("sessionId", ctxSessionId))
+        }
+    }
+    return newLogger
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module benelog
+
+go 1.15
+
+require (
+	github.com/pborman/uuid v1.2.1 // indirect
+	go.uber.org/zap v1.19.0 // indirect
+)
